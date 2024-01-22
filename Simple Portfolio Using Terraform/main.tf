0490aee16f514e541e9a9fdf2247764a6604ceb9
AkashShendkar <155044678+AkashShendkar@users.noreply.github.com>
2024-01-22 10:44:11 +0530

    Create main.tf

---
+++ Simple Portfolio Using Terraform/main.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "server1" {
+  ami = var.ami_value
+  instance_type = var.instance_type_value
+  key_name = var.key_name
+  tags = {
+    Name = "server1"
+  }
+  user_data = base64encode(file("userdata.sh"))
+}
