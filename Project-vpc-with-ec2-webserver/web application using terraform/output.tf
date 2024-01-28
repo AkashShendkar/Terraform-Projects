5f7cd1d03a66a50787e53be3a8db8a27118f48ea
AkashShendkar <155044678+AkashShendkar@users.noreply.github.com>
2024-01-28 09:56:08 +0530

    Delete Project-vpc-with-ec2-webserver/web application using terraform directory

--- Project-vpc-with-ec2-webserver/web application using terraform/output.tf
+++
@@ -1,9 +0,0 @@
-output  "public_ip" {
-   value = aws_instance.webserver1.public_ip 
-}
-output  "public_ip2" {
-   value = aws_instance.webserver2.public_ip 
-}
-output "public_dns" {
-  value = aws_lb.myalb.dns_name
-}
