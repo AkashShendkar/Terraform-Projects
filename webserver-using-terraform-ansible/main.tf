2ab5a6503081bcd8826a13bdc30a8bcdabb397f0
Akash Shendkar <155044678+AkashShendkar@users.noreply.github.com>
2024-02-18 10:34:02 +0530

    Update main.tf

--- webserver-using-terraform-ansible/main.tf
+++ webserver-using-terraform-ansible/main.tf
@@ -6,6 +6,7 @@ resource "aws_instance" "server1" {
   ami = var.ami_value
   instance_type = var.instance_type_value
   key_name = var.key_name
+  security_groups = var.security_groups
   
   tags = {
     Name = "server1"
